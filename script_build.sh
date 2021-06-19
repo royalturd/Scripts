264881f0e8df31d37afa29080d5f39aab66ef195
royalturd <54993556+royalturd@users.noreply.github.com>
2021-06-19 22:35:32 +0530

    Update script_build.sh

--- script_build.sh
+++ script_build.sh
@@ -28,7 +28,7 @@ echo -e ${blu}"CCACHE is enabled for this build"${txtrst}
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export CCACHE_DIR=/home/ccache/$username
-ccache -M 50G
+ccache -M 150G
 fi
 
 if [ "$use_ccache" = "clean" ];
@@ -37,7 +37,7 @@ export CCACHE_EXEC=$(which ccache)
 export CCACHE_DIR=/home/ccache/$username
 ccache -C
 export USE_CCACHE=1
-ccache -M 50G
+ccache -M 150G
 wait
 echo -e ${grn}"CCACHE Cleared"${txtrst};
 fi
